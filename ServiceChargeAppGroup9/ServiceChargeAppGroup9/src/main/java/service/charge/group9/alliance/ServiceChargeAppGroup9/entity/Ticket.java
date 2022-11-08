4eefe2fa9401dabba90e3da14c8d413a5a91f649
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2022-11-08 21:00:18 +0800

    initial setup

---
+++ ServiceChargeAppGroup9/ServiceChargeAppGroup9/src/main/java/service/charge/group9/alliance/ServiceChargeAppGroup9/entity/Ticket.java
@@ -0,0 +1,5 @@
+package service.charge.group9.alliance.ServiceChargeAppGroup9.entity;
+
+public class Ticket {
+
+}
---
+++ ServiceChargeAppGroup9/ServiceChargeAppGroup9/src/main/java/service/charge/group9/alliance/ServiceChargeAppGroup9/service/TicketService.java
@@ -0,0 +1,5 @@
+package service.charge.group9.alliance.ServiceChargeAppGroup9.service;
+
+public class TicketService {
+
+}
