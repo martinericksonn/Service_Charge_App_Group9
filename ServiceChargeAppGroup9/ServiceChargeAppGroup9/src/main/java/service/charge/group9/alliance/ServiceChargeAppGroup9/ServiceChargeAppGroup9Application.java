3b797b29464ef20166f33214084d8d9856e24ca3
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2022-11-08 20:41:37 +0800

    spring initializr

---
+++ ServiceChargeAppGroup9/ServiceChargeAppGroup9/src/main/java/service/charge/group9/alliance/ServiceChargeAppGroup9/ServiceChargeAppGroup9Application.java
@@ -0,0 +1,13 @@
+package service.charge.group9.alliance.ServiceChargeAppGroup9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceChargeAppGroup9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceChargeAppGroup9Application.class, args);
+	}
+
+}
---
+++ ServiceChargeAppGroup9/ServiceChargeAppGroup9/src/test/java/service/charge/group9/alliance/ServiceChargeAppGroup9/ServiceChargeAppGroup9ApplicationTests.java
@@ -0,0 +1,13 @@
+package service.charge.group9.alliance.ServiceChargeAppGroup9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceChargeAppGroup9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
