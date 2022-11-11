ab0343297ec8c6fb0b6b0080bcc9b5f1d5c87360
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-11-11 21:46:31 +0800

    add ticket controller methods

--- Frontend/service-charge-app/src/Components/Dashboard.jsx
+++ Frontend/service-charge-app/src/Components/Dashboard.jsx
@@ -7,18 +7,18 @@ import '../index.css';
     
     render() { 
         return (
-          <div className="nav-side">
+          <div className="d-flex flex-row body bg-primary">
+          <div className="side-nav bg-info ">
             <div className='d-flex flex-column align-items-center'>
               <h2>Menu</h2>
               <div className='d-flex flex-column '>
-
               <button className='btn btn-outline-primary'>Home</button>
               <button className='btn btn-outline-primary'>Ticket</button>
               <button className='btn btn-outline-primary'>Settings</button>
               </div>
             </div>
-
 </div>
+          </div>
         );
     }
  }
