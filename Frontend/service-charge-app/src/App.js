47935331aa4da08584a22a1ef78119c74659c749
patchiepies <patcustodio31@gmail.com>
2022-11-11 18:44:36 +0800

    add sidebar items

--- Frontend/service-charge-app/src/App.js
+++ Frontend/service-charge-app/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import TicketManagement from './Components/TicketManagement';
 import Dashboard from './Components/Dashboard';
+import Sidebar from './Components/sidebar/Sidebar';
 
 function App() {
     return ( 
       <div>
-        
+        <Sidebar/>
         <Dashboard/>
       </div>
     );
