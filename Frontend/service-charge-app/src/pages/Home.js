529558123afa0fdbd0161ee7fa836f2060cf8a3c
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-11-19 10:38:00 +0800

    react axios connection

--- Frontend/service-charge-app/src/pages/Home.js
+++ Frontend/service-charge-app/src/pages/Home.js
@@ -1,5 +1,9 @@
 export default function Home(){
+
     return(
         <h1>Home</h1>
+
     )
+
+    
 }
--- Frontend/service-charge-app/src/pages/TicketList.js
+++ Frontend/service-charge-app/src/pages/TicketList.js
@@ -1,5 +1,22 @@
+
+import axios, * as others from 'axios';
+import React from "react";
+const baseURL = "localhost:8080/ticket/all"
 export default function TicketList(){
-    return(
-        <h1>Ticket List</h1>
-    )
+    const [post, setPost] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+          setPost(response.data);
+        });
+      }, []);
+    
+      if (!post) return null;
+    
+      return (
+        <div>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      );
 }
