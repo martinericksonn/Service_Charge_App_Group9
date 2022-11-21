702517ad364d218b5bd04d2cdb3e331517b175dc
Patchiepies <patcustodio31@gmail.com>
2022-11-21 15:30:04 +0800

    update Login Page
    still have errors

--- Frontend/service-charge-app/src/App.js
+++ Frontend/service-charge-app/src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react'; import './App.css';
+import { useState } from 'react'; 
+import './App.css';
+import './loginform.css';
 import TicketManagement from './Components/TicketManagement';
-import LogIn from './Components/LogIn';
 import Navbar from './Components/Navbar';
 import TicketList from './pages/TicketList';
 import NewTicket from './pages/NewTicket';
@@ -8,14 +9,52 @@ import Notification from './pages/Notification';
 import Attachments from './pages/Attachments';
 import ViewTicket from './pages/ViewTicket';
 import {Route, Routes} from "react-router-dom"
+import LoginForm from './Components/LoginForm';
 
 
 function App() {
-    const [currentForm, setCurrentForm] = useState ('login');
+    const adminUser ={
+        email: "admin@admin.com",
+        password:"admin123"
+    }
+    const[user, setUser] = useState({name:"",email:""});
+    const[error, setError] = useState("");
+
+    const Login = details =>{
+        console.log(details);
+
+        if(details.email == adminUser.email && details.password == adminUser.password){
+            console.log("Logged In!");
+            setUser({
+                name: details.name,
+                email: details.email,
+                password: details.password
+            })
+        }
+        else{
+            console.log("Does not match");
+            setError("Details do not match!");  
+        }
+
+    }
+    
+    const Logout = () =>{
+        setUser({name:"",email:""})
+    }
+
     return ( 
         <>
-        <LogIn/>
-        <Navbar/>
+        <div className='App'>
+            {(user.email!="") ?(
+                <div className='welcome'>
+                    <h2> welcome, <span>{user.name}</span></h2>
+                    <button onClick={Logout}>Logout</button>
+                </div>
+            ): (
+                <LoginForm Login={Login} error={error}/>
+            )}
+        </div>
+        <Navbar/> 
         <div className="container">
             <Routes>
                 <Route path='/Home' element={<App/>}/>
