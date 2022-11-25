35ce082f29cce5a6111261ca79bcedddc44d827e
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-11-25 22:54:40 +0800

    GlobalCors

---
+++ ServiceChargeAppGroup9/ServiceChargeAppGroup9/src/main/java/service/charge/group9/alliance/ServiceChargeAppGroup9/config/GlobalCorsConfig.java
@@ -0,0 +1,17 @@
+package service.charge.group9.alliance.ServiceChargeAppGroup9.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200").allowedHeaders("*").allowCredentials(false)
+				.maxAge(-1);
+	}
+}
