a7250f740cc9c5fb6eb24a483e7add8c1389ec1a
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-12-08 01:22:31 +0800

    working select role dropdown

--- Frontend/service_charge_app_flutter/lib/src/widgets/createTicket.dart
+++ Frontend/service_charge_app_flutter/lib/src/widgets/createTicket.dart
@@ -15,7 +15,6 @@ class CreateTicket extends StatelessWidget {
   Widget build(BuildContext context) {
     TextEditingController forDescription = TextEditingController();
     TextEditingController forSubject1 = TextEditingController();
-    TextEditingController forSubject2 = TextEditingController();
 
     return Container(
       margin: EdgeInsets.only(left: 300, right: 300, top: 20),
@@ -38,170 +37,191 @@ class CreateTicket extends StatelessWidget {
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 80,
-                            child: Text(
-                              "Subject",
-                              style: TextStyle(
-                                fontSize: 12,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: TextField(
-                              controller: forSubject1,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.grey),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    subject(forSubject1),
                     SizedBox(
                       height: 10,
                     ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 80,
-                          child: Text(
-                            "Description",
-                            style: TextStyle(
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            maxLines: 5,
-                            controller: forDescription,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      // color: Colors.red,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        // ignore: prefer_const_literals_to_create_immutables
-                        children: [
-                          SizedBox(
-                            width: 40,
-                          ),
-                          Row(
-                            children: const [
-                              Text(
-                                "Status",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                              TixStatusDropDown(),
-                            ],
-                          ),
-                          // SizedBox(width: 105),
-                          Row(
-                            children: const [
-                              Text(
-                                "Select Role",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                              RolesDropdown(),
-                            ],
-                          ),
-
-                          Row(
-                            children: const [
-                              Text(
-                                "Select Assignee",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                ),
-                              ),
-                              AssigneeDropDown(),
-                            ],
-                          ),
-                          SizedBox(
-                            width: 40,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: [
-                        SizedBox(
-                          width: 40,
-                        ),
-                        Row(
-                          children: const [
-                            Text(
-                              "Date Started",
-                              style: TextStyle(
-                                fontSize: 12,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 150,
-                              child: DatePicker(),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: 150,
-                          child:  FilePcker(),
-                        ),
-                        SizedBox(
-                          width: 40,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: [
-                        SizedBox(width: 125),
-                      ],
-                    ),
+                    description(forDescription),
+                    roleAssignee(),
+                    statusAttachFile(),
                     SizedBox(
                       height: 10,
                     ),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      height: 60,
-                      width: 100,
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.blue,
-                            foregroundColor: Colors.white),
-                        onPressed: () {
-                          forDescription.clear();
-                          forSubject1.clear();
-                          forSubject2.clear();
-                        },
-                        child: Text("Create"),
-                      ),
+                    createButton(
+                      forDescription,
+                      forSubject1,
                     ),
                   ]),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Container createButton(
+    TextEditingController forDescription,
+    TextEditingController forSubject1,
+  ) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 60,
+      width: 100,
+      child: TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.blue, foregroundColor: Colors.white),
+        onPressed: () {
+          print(forDescription.text);
+          print(forSubject1.text);
+          forDescription.clear();
+          forSubject1.clear();
+        },
+        child: Text("Create"),
+      ),
+    );
+  }
+
+  Widget statusAttachFile() {
+    return SizedBox(
+      height: 45,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            width: 40,
+          ),
+          Row(
+            children: const [
+              Text(
+                "Status",
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              TixStatusDropDown(),
+            ],
+          ),
+          // Row(
+          //   children: const [
+          //     Text(
+          //       "Date Started",
+          //       style: TextStyle(
+          //         fontSize: 12,
+          //       ),
+          //     ),
+          //     SizedBox(
+          //       width: 150,
+          //       child: DatePicker(),
+          //     ),
+          //   ],
+          // ),
+          SizedBox(
+            width: 150,
+            child: FilePcker(),
+          ),
+          SizedBox(
+            width: 40,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget roleAssignee() {
+    return SizedBox(
+      height: 45,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          SizedBox(
+            width: 40,
+          ),
+
+          // SizedBox(width: 105),
+          Row(
+            children: const [
+              Text(
+                "Select Role",
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              RolesDropdown(),
+            ],
+          ),
+
+          Row(
+            children: const [
+              Text(
+                "Select Assignee",
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              AssigneeDropDown(),
+            ],
+          ),
+          SizedBox(
+            width: 40,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row description(TextEditingController forDescription) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 80,
+          child: Text(
+            "Description",
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            maxLines: 5,
+            controller: forDescription,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row subject(TextEditingController forSubject1) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 80,
+          child: Text(
+            "Subject",
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            controller: forSubject1,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- Frontend/service_charge_app_flutter/lib/src/widgets/rolesDropdown.dart
+++ Frontend/service_charge_app_flutter/lib/src/widgets/rolesDropdown.dart
@@ -1,52 +1,65 @@
-
 import 'package:flutter/material.dart';
+import 'package:service_charge_app/src/controller/role_controller.dart';
+import 'package:service_charge_app/src/entity/role/Role.dart';
 
-
-  List<String> roles = [
-    "CEO",
-    "COO",
-    "Accountant",
-    "HR",
-    "Manager",
-    "Head HR",
-     "--",
-  ];
+// List<String> roles = [
+//   "CEO",
+//   "COO",
+//   "Accountant",
+//   "HR",
+//   "Manager",
+//   "Head HR",
+//   "--",
+// ];
 
 class RolesDropdown extends StatefulWidget {
-  const RolesDropdown ({super.key,});
+  const RolesDropdown({
+    super.key,
+  });
 
   @override
   State<RolesDropdown> createState() => _RolesDropdownState();
 }
 
 class _RolesDropdownState extends State<RolesDropdown> {
-String rolesID = roles.last;
-
+  RoleController roleController = RoleController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(12.0),
-        child: SizedBox(
-          height: 50,
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton(
-              onChanged: (String? value) {
-               setState(() {
-                rolesID = value!;
+    return FutureBuilder<List<Role>>(
+        future: roleController.getAllRole(),
+        builder: (BuildContext context, AsyncSnapshot<List<Role>> snapshot) {
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+
+          var roleList = snapshot.data!;
+          var roles = roleList.map((e) => e.role).toList();
+          roles.add('--');
+          var rolesID = roles.last;
+
+          return Container(
+            padding: const EdgeInsets.all(12.0),
+            child: SizedBox(
+              height: 50,
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton(
+                  onChanged: (String? value) {
+                    setState(() {
+                      rolesID = value!;
                     });
-                },
-              value: rolesID,
-              items: roles.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-            
+                  },
+                  value: rolesID,
+                  items: roles.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
             ),
-          ),
-        ),
-      );
+          );
+        });
   }
 }
